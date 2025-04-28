0a36856a3fba14d99aa0034be3eaea9edb11e11b
Denisemorini123 <Dmanuelemorini@gmail.com>
2025-04-28 14:22:28 -0300

    evidencias de teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,14 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://127.0.0.1:8080',
+    baseUrl: 'http://127.0.0.1:8080/',
+    video: true, // Gravar vídeos dos testes
     setupNodeEvents(on, config) {
       // você pode configurar plugins aqui se precisar
     },
-    baseUrl: 'http://127.0.0.1:8080/'
   },
-};
+});
+
+
 
